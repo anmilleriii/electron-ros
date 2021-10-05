a8c58adf668603ae7b85ed60aa76ce99d9d798f0
Al Miller <albert.miller@fireflyautomatix.com>
2021-10-04 23:03:21 -0700

    fix(package): uncomment

--- view/src/ros/index.js
+++ view/src/ros/index.js
@@ -1,28 +1,28 @@
-// const rclnodejs = require("rclnodejs");
-// const { Context } = require("rclnodejs");
-// const sampleTopicSubscription = require("./modules/subscriber.js");
+const rclnodejs = require("rclnodejs");
+const { Context } = require("rclnodejs");
+const sampleTopicSubscription = require("./modules/subscriber.js");
 
-// const options = {
-//   cors: {
-//     origin: `*`,
-//     methods: ["GET", "POST"],
-//   },
-// };
-// const io = require("socket.io")(options);
-// const onConnection = (socket) => {
-//   rclnodejs
-//     .init()
-//     .then(() => {
-//       sampleTopicSubscription.default(socket);
-//       rclnodejs.spin(node);
-//     })
-//     .catch((error) => console.log(error));
+const options = {
+  cors: {
+    origin: `*`,
+    methods: ["GET", "POST"],
+  },
+};
+const io = require("socket.io")(options);
+const onConnection = (socket) => {
+  rclnodejs
+    .init()
+    .then(() => {
+      sampleTopicSubscription.default(socket);
+      rclnodejs.spin(node);
+    })
+    .catch((error) => console.log(error));
 
-//   io.on("disconnect", (socket) => {
-//     rclnodejs.shutdownAll();
-//     socket.diconnect(true);
-//   });
-// };
-// io.on("connection", onConnection);
+  io.on("disconnect", (socket) => {
+    rclnodejs.shutdownAll();
+    socket.diconnect(true);
+  });
+};
+io.on("connection", onConnection);
 
-// io.listen(4545);
+io.listen(4545);
