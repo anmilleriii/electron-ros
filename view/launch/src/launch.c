f14616d69156b40712dcc5d946bec3bf184b2edc
Al Miller <albert.miller@fireflyautomatix.com>
2021-10-05 09:07:31 -0700

    interim

--- view/launch/src/launch.c
+++ view/launch/src/launch.c
@@ -2,7 +2,9 @@
 
 int main() {
     // putenv("PS1=BobeTerm");
-    system("bash -i -c \"source /opt/ros/foxy/setup.bash && ./editor\"");
+    // see: https://stackoverflow.com/questions/43659084/source-bashrc-in-a-script-not-working?rq=1
+    system("bash -i -c \"source /opt/ros/foxy/setup.bash && ros2\"");
+    // system("bash -i -c \"source /opt/ros/foxy/setup.bash && ./editor\"");
 
 
     return 0;
