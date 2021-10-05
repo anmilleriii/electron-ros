02e1f439dfe8a543d1659234eb9cc97c4c6fbd6a
Al Miller <albert.miller@fireflyautomatix.com>
2021-10-04 23:02:01 -0700

    fix(package): add package details

--- view/vue.config.js
+++ view/vue.config.js
@@ -1,14 +1,12 @@
 module.exports = {
-
   pluginOptions: {
     electronBuilder: {
       nodeIntegration: true,
-    //   externals: ['json-server', 'rclnodejs'],
-        linux: {
-          "target": "AppImage",
-          "category": "Utility"
-        },
+      externals: ["rclnodejs"],
+      linux: {
+        target: "AppImage",
+        category: "Utility",
       },
     },
   },
-}
+};
