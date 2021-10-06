115ae31d009f2abe286d3bc39d011c0e03ba4867
Al Miller <albert.miller@fireflyautomatix.com>
2021-10-05 17:55:22 -0700

    interim

--- app/src/renderers/ros/modules/publisher.js
+++ app/src/renderers/ros/modules/publisher.js
@@ -8,6 +8,8 @@ const createTopicPublisher = (socket) => {
   const node = new rclnodejs.Node("publisher_example_node");
   const publisher = node.createPublisher("std_msgs/msg/String", "topic");
   publisher.publish(`Hello ROS 2 from rclnodejs`);
+  console.clear()
+  console.log('creating publisher')
   rclnodejs.spin(node);
 };
 
--- app/src/renderers/ros/modules/subscriber.js
+++ app/src/renderers/ros/modules/subscriber.js
@@ -4,8 +4,10 @@ const createTopicSubscription = (socket) => {
   const node = rclnodejs.createNode(`subscriber_example_node`);
 
   node.createSubscription("std_msgs/msg/String", "topic", (message) => {
+    // only listen at throttled interval
     setInterval(() => {
       socket.emit("/topic", message);
+      console.log('emitting msg', message)
       console.log("message from publisher:", message);
     }, 1000);
   });
