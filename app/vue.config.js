3946dc1ce1b945e4432ae98f1d2d6c21171f25d5
Al Miller <albert.miller@fireflyautomatix.com>
2021-10-05 17:39:32 -0700

    interim

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,6 +1,19 @@
 module.exports = {
+
+  configureWebpack: {
+    // Define entrypoint for the Vue application (not the broader Electron app entrypoint)
+    entry: "./src/renderers/view/main.js",
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src/renderers/view/')
+      }
+    }
+  },
+
   pluginOptions: {
     electronBuilder: {
+      // Specify entrypoint for Electron app
+      mainProcessFile: 'src/main/background.js',
       nodeIntegration: true,
       externals: ["rclnodejs"],
       linux: {
